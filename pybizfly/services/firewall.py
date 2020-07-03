58c5cfd69ba980a4dabdae310f4e0e6f85759219
dungpq <dungpq@vccloud.vn>
2020-07-03 12:28:45 +0700

    feat: firewall methods

--- pybizfly/services/firewall.py
+++ pybizfly/services/firewall.py
@@ -1,20 +1,39 @@
 from constants.api import ENDPOINTS
 from services.segregations import Service, Listable, Gettable, Creatable, Patchable, Deletable
+from utils.validators import validate_str_list
 
 
 class Firewall(Listable, Gettable, Creatable, Patchable, Deletable):
+    def get(self, firewall_id: str, *args, **kwargs) -> Service:
+        return super(Firewall, self).get(firewall_id)
+
     def create(self, name: str,
-               inbound: list,
-               outbound: list,
-               on_services: list,
-               *args, **kwargs) -> 'Service':
+               inbound_rules: list, outbound_rules: list,
+               on_services: list, *args, **kwargs) -> Service:
+        self._request_body = self.__generate_create_firewall_request_body(**locals())
         return super(Firewall, self).create()
 
-    def update(self, firewall_id: str, *args, **kwargs) -> Service:
+    def update_rules(self, firewall_id: str,
+                     inbound_rules: list, outbound_rules: list,
+                     on_services: list, *args, **kwargs) -> Service:
+        self._request_body = self.__generate_update_firewall_request_body(**locals())
         return super(Firewall, self).update(firewall_id)
 
+    def delete(self, firewall_id: str, *args, **kwargs) -> Service:
+        return super(Firewall, self).delete(firewall_id)
+
+    def delete_across_servers(self, firewall_id: str, servers: list) -> Service:
+        service = super(Firewall, self).delete(firewall_id)
+        self._add_sub_endpoint('servers')
+
+        servers = validate_str_list(servers)
+        self._request_body = {
+            'servers': servers
+        }
+        return service
+
     @staticmethod
-    def __generate_create_firewall_request_body() -> dict:
+    def __generate_create_firewall_request_body(**kwargs) -> dict:
         return {
             "name": "bizflycloud-firewall",
             "inbound": [
@@ -45,6 +64,26 @@ class Firewall(Listable, Gettable, Creatable, Patchable, Deletable):
                  "cidr": "192.168.0.0/28"
                  }
 
+            ],
+            "targets": ["26b5cb61-95bb-417e-a1a3-2ea51f40d6ee"]
+        }
+
+    @staticmethod
+    def __generate_update_firewall_request_body(**kwargs) -> dict:
+        return {
+            "inbound": [
+                {"type": "SSH",
+                 "protocol": "TCP",
+                 "port_range": "22",
+                 "cidr": "0.0.0.0/0"
+                 },
+                {"type": "HTTP",
+                 "protocol": "TCP",
+                 "port_range": "80",
+                 "cidr": "10.5.68.111"
+                 },
+            ],
+            "outbound": [
             ],
             "targets": ["26b5cb61-95bb-417e-a1a3-2ea51f40d6ee"]
         }
