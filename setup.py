ccb12afb3feed5bc994ef63a05b114cfe670572a
dungpq <phanquangdng@gmail.com>
2021-01-15 11:16:08 +0700

    feat: update setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="BizFly Client in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/bizflycloud/pybizfly",
     packages=setuptools.find_packages(),
     install_requires=[
         'requests>=2.24.0',
