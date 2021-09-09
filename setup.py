614bc920681fe219daa25cd25ef53cad87b1b874
tung491 <dosontung007@gmail.com>
2021-09-09 10:02:09 +0700

    feat: raise HTTP exception when response not ok

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="pybizfly",  # Replace with your own username
-    version="0.2.0",
+    version="0.2.1",
     author="BizFly Cloud",
     author_email="dungpq@vccloud.vn",
     description="BizFly Client in Python",
